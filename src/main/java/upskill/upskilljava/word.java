72dc05f01f0b75429966e8ad13c316cb4d1e9773
Bárbara Leston Bandeira <blestonbandeira@upskill.pt>
2020-11-17 16:03:14 +0000

    classe CursoWord actualizada

--- src/main/java/upskill/upskilljava/word.java
+++ src/main/java/upskill/upskilljava/word.java
@@ -13,24 +13,6 @@ import java.util.List;
  * @author prp
  */
 public class Word extends curso {
+
     
-    private String nome;
-    private int numeroFormandos;
-
-    public curso(String n, int num){
-        this.nome=DefNome(n);
-        this.numeroFormandos=DefNumFormandos(num);
-    }
-
-    private String DefNome(String nome)
-    {
-        return nome.trim();
-    }
-
-    private int DefNumFormandos(int num){
-        if num<0 
-            return 0;
-        else
-            return num;
-    }
 }
