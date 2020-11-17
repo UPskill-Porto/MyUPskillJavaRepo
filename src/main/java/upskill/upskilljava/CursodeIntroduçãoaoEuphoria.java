852699f79617a18a9a2709c2d7f355352be094a9
João Ribeiro <joaomribeiro95@upskill.pt>
2020-11-17 15:47:57 +0000

    Criação da class CursodeIntroduçãoaoEuphoria

---
+++ src/main/java/upskill/upskilljava/CursodeIntroduçãoaoEuphoria.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package upskill.upskilljava;
+
+/**
+ *
+ * @author Joao
+ */
+public class CursodeIntroduçãoaoEuphoria extends curso {
+    
+    public CursodeIntroduçãoaoEuphoria(String n, int num) {
+        super(n, num);
+    }
+    
+}
